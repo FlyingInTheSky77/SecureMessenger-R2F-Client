e79cffde9a7da8edcc6332d3138b3b3c4ba24380
dragon <dima_rodin@yahoo.com>
2025-04-10 23:49:14 +0300

    add user status in contact list #9
    - added user status in contact list
    - check if client's contact list are bigger that contact server list
    - and mark contacts missing from the server as deleted
    
    ISSUE: Client GUI#4

--- include/SqlContactModel.h
+++ include/SqlContactModel.h
@@ -9,4 +9,6 @@ public:
     SqlContactModel( QObject *parent = nullptr );
     Q_INVOKABLE void refresh();
 
+    QHash<int, QByteArray> roleNames() const;
+    QVariant data(const QModelIndex &index, int role) const;
 };
--- src/SqlContactModel.cpp
+++ src/SqlContactModel.cpp
@@ -29,3 +29,37 @@ void SqlContactModel::refresh()
     if ( lastError().isValid() )
         qFatal( "Cannot set query on SqlContactModel: %s", qPrintable(lastError().text() ) );
 }
+
+QHash<int, QByteArray> SqlContactModel::roleNames() const
+{
+    QHash<int, QByteArray> roles;
+    roles[Qt::DisplayRole] = "display";
+    roles[Qt::UserRole + 1] = "name";
+    roles[Qt::UserRole + 2] = "activityStatus";
+    return roles;
+}
+
+QVariant SqlContactModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid())
+        return QVariant();
+
+    if (role == Qt::DisplayRole) {
+            QString name = QSqlQueryModel::data(this->index(index.row(), 0)).toString();
+            QString status = QSqlQueryModel::data(this->index(index.row(), 1)).toString();
+            if ( status == "1" )  {
+                status = "online";
+            }
+            else if ( status == "0" ) {
+                status = "offline";
+            }
+            return QString("<b>%1</b> <span style='color:gray;'>[ status - %2 ]</span>").arg(name, status);
+        }
+    else if (role == Qt::UserRole + 1) {
+        return QSqlQueryModel::data(this->index(index.row(), 0)); // name
+    } else if (role == Qt::UserRole + 2) {
+        return QSqlQueryModel::data(this->index(index.row(), 1)); // activityStatus
+    }
+
+    return QVariant();
+}
